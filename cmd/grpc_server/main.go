38721cbb3752cadc1126c00e2d3d07362810a764
Igor Popov <spartak-992010@yandex.ru>
2025-05-14 18:40:02 +0600

    add grpc methods

--- cmd/grpc_server/main.go
+++ cmd/grpc_server/main.go
@@ -1,19 +1,60 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 
+	"github.com/brianvoe/gofakeit"
 	desc "github.com/igorezka/auth/pkg/user_v1"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 const grpcPort = 50051
 
 type server struct {
 	desc.UnimplementedUserV1Server
+}
+
+func (*server) Create(_ context.Context, req *desc.CreateRequest) (*desc.CreateResponse, error) {
+	log.Printf("User name: %s", req.GetInfo().GetName())
+
+	return &desc.CreateResponse{
+		Id: gofakeit.Int64(),
+	}, nil
+}
+
+func (*server) Get(_ context.Context, req *desc.GetRequest) (*desc.GetResponse, error) {
+	log.Printf("User id: %d", req.GetId())
+
+	return &desc.GetResponse{
+		User: &desc.User{
+			Id: gofakeit.Int64(),
+			Info: &desc.UserInfo{
+				Name:  gofakeit.Name(),
+				Email: gofakeit.Email(),
+				Role:  0,
+			},
+			CreatedAt: timestamppb.New(gofakeit.Date()),
+			UpdatedAt: timestamppb.New(gofakeit.Date()),
+		},
+	}, nil
+}
+
+func (*server) Update(_ context.Context, req *desc.UpdateRequest) (*emptypb.Empty, error) {
+	log.Printf("user id: %d", req.GetId())
+
+	return &emptypb.Empty{}, nil
+}
+
+func (*server) Delete(_ context.Context, req *desc.DeleteRequest) (*emptypb.Empty, error) {
+	log.Printf("user id: %d", req.GetId())
+
+	return &emptypb.Empty{}, nil
 }
 
 func main() {
